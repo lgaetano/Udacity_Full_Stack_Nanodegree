ca35c258c24da2bc6060643089b4279611b6d1cb
Lia Gaetano <LiaG@Lias-MacBook-Pro.local>
2021-04-22 12:28:59 -0700

    Created test_get_questions and test_get_categories

--- trivia_api/backend/test_flaskr.py
+++ trivia_api/backend/test_flaskr.py
@@ -15,10 +15,11 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        # self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = 'postgresql://postgres:1234@localhost:5432/trivia'
         setup_db(self.app, self.database_path)
 
-        # binds the app to the current context
+        # Binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
@@ -34,6 +35,36 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_questions(self):
+        '''Testing question retrieval.'''
+
+        res = self.client().get('/questions?page=1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['sucess'], True)
+
+        self.assertTrue(data['questions'])
+        self.assertIsInstance(data['questions'], list)
+        self.assertEqual(len(data['questions'], 10))
+        self.assertEqual(data['total_questions'], 19)
+
+        self.assertTrue(data['categories'])
+        self.assertEqual(len(data['categories']), 6)
+        self.assertIsInstance(data['categories'], dict)
+        self.assertEqual(data['current_category'], None)
+
+    def test_get_questions(self):
+        ''' Testing category retrieval'''
+
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertEqual(len(data['categories']), 6)
+        self.assertIsInstance(data['categories'], dict)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
