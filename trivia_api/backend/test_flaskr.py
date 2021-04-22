60b48cec9c494eaed3e23fdb14328693ff3c0182
Lia Gaetano <LiaG@Lias-MacBook-Pro.local>
2021-04-22 14:23:23 -0700

    Added test cases

--- trivia_api/backend/test_flaskr.py
+++ trivia_api/backend/test_flaskr.py
@@ -66,6 +66,63 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(len(data['categories']), 6)
         self.assertIsInstance(data['categories'], dict)
 
+    def test_get_questions_by_category(self):
+        '''Testing retrieval of questions by category'''
+
+        res = self.client().get('/categories/1/questions?page=1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        
+        self.assertTrue(data['questions'])
+        self.assertIsInstance(data['questions'], list)
+        self.assertEqual(len(data['questions']), 3)
+        self.assertEqual(data['total_questions'], 3)
+        self.assertEqual(data['current_category'], 1)
+
+    def test_delete_question(self):
+        '''Testing question deletion.'''
+
+        res = self.client().delete('/questions/4')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 4)
+
+    def test_search_questions(self):
+        '''Testing search endpoint.'''
+
+        res = self.client().post('/search', json={'searchTerm': 'soccer'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        self.assertTrue(data['questions'])
+        self.assertIsInstance(data['questions'], list)
+        self.assertEqual(len(data['questions']), 1)
+        self.assertEqual(data['total_questions'], 1)
+        self.assertEqual(data['current_category'], None)
+
+    def test_search_question_no_result(self):
+        '''Search for a question where there is no result'''
+
+        res = self.client().post('/search', json={'searchTerm': 'blamo'})
+        data = json.load(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], True)
+
+        self.assertIsInstance(data['questions'], list)
+        self.assertEqual(len(data['questions']), 0)
+        self.assertEqual(data['total_questions'], 0)
+        self.assertEqual(data['current_category'], None)
+    
+
+
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
