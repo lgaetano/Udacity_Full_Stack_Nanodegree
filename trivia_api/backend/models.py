93de32735f3056297f13112eee29cc5f4a240d28
Lia Gaetano <LiaG@Lias-MacBook-Pro.local>
2021-04-22 12:27:19 -0700

    Corrected database path

--- trivia_api/backend/models.py
+++ trivia_api/backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = 'postgres://postgres:1234@localhost:5432/trivia'
+database_path = 'postgresql://postgres:1234@localhost:5432/trivia'
 
 
 db = SQLAlchemy()
